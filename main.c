3c6ed50f5d5d01f3312edcf281c5f2701bb34e95
pjs95 <geotradepopov@gmail.com>
2019-06-01 20:08:11 +0300

    update

--- main.c
+++ main.c
@@ -10,12 +10,42 @@
 #include <util/delay.h>
 #include <string.h>
 #include <stdio.h>
-#include "lightf.h"
+#include "lightfAndMore.h"
 
 
 int main(void)
 {	
-	char inputMorse[128] = ".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--"; //Hello world!
+	DDRB |= (1 << DDB0);
+	DDRD |= (1 << DDD0)|(1 << DDD1)|(1 << DDD4); 
+	
+
+while(1)
+	{
+	char inputMorse[128] = {0};//".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--"; //Hello world!
+	short orderN = 0;
+	short MSECONDS = 5000;
+	if (PIND0&0b00000001 || PIND1&0b00000010 || PIND4&0b00010000)
+	{
+		while (orderN < 128 && waitTime(MSECONDS))
+		{
+			if (PIND0&0b00000001)
+			{
+				inputMorse[orderN++] = '.';
+				MSECONDS = 5000;
+			}
+			else if (PIND1&0b00000010)
+			{
+				inputMorse[orderN++] = '-';
+				MSECONDS = 5000;
+			}
+			else if (PIND4&0b00010000)
+			{
+				inputMorse[orderN++] = ' ';
+				MSECONDS = 5000;
+			}
+		}
+	}
+}
 	
 	lightf(inputMorse);		
 }
