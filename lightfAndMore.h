047527e2dd846aa75911bbd1c196c1dd36408fb5
pjs95 <geotradepopov@gmail.com>
2019-06-01 20:10:29 +0300

    Add files via upload

---
+++ lightfAndMore.h
@@ -0,0 +1,45 @@
+/*
+ * Input to Light Function
+ * 
+ * Created: 5/30/2019
+ * Author : PJS
+ */ 
+void lightf (char morseCode[128])
+{
+	short morseUnit = 128;
+	
+	//PORTB |= (1 << PORTB0); //Turns off the diode.
+	//PORTB &= ~(1 << PORTB0); //Turns on the diode.
+	
+	for (short j = 0; j < strlen(morseCode); j++)
+	{
+		if (morseCode[j] == '.')
+		{
+			PORTB &= ~(1 << PORTB0);
+			_delay_ms(morseUnit);
+			
+			PORTB |= (1 << PORTB0);
+			_delay_ms(morseUnit);
+		}
+		else if (morseCode[j] == ' ')
+		_delay_ms(2*morseUnit);
+		else if (morseCode[j] == '/')
+		_delay_ms(3*morseUnit);
+		else if (morseCode[j] == '-')
+		{
+			PORTB &= ~(1 << PORTB0);
+			_delay_ms(3*morseUnit);
+			PORTB |= (1 << PORTB0);
+			_delay_ms(morseUnit);
+		}
+	}
+}
+
+int waitTime(int mSeconds)
+{
+	for (long iterations = 550000 * mSeconds; iterations > 0; )
+	{
+		 iterations -= 1;
+	}
+	return 0;
+}
